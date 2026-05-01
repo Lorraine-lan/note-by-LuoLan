055a96b06c44c04c9546b10ea3532f119f6a2fd2
Lorraine-lan <3180868806@qq.com>
2026-05-01 15:23:49 +0800

    Delete supermarket directory

--- supermarket/src/main/java/com/hgsf/supermarket/service/RoleService.java
+++
@@ -1,7 +0,0 @@
-package com.hgsf.supermarket.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.hgsf.supermarket.entity.Role;
-
-public interface RoleService extends IService<Role> {
-}
